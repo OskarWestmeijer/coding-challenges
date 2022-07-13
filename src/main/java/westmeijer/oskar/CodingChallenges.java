16a17de0db1df88ee90ce9518c6469b1886fd2f3
Oskar Westmeijer <oskar.westmeijer@s24.com>
2022-07-13 12:57:18 +0200

    create maven project

---
+++ src/main/java/westmeijer/oskar/CodingChallenges.java
@@ -0,0 +1,7 @@
+package westmeijer.oskar;
+
+public class CodingChallenges {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/westmeijer/oskar/CodingChallengesTest.java
@@ -0,0 +1,15 @@
+package westmeijer.oskar;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CodingChallengesTest {
+
+    @Test
+    public void simpleTest() {
+        Assertions.assertEquals(3, 1 + 2);
+
+    }
+
+}
